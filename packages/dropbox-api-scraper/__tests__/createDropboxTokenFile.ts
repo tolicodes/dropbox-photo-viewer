08d3eec96281e31a616a0394de6d3217f86ce036
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-04-20 14:53:19 +0800

    Template

--- packages/dropbox-api-scraper/__tests__/createDropboxTokenFile.ts
+++
@@ -1,7 +0,0 @@
-describe("@cloud-photo-viewer/dropbox-api-scraper/createDropboxTokenFile", () => {
-  it.todo("should generate a token using oauth-cli and save it to a file");
-  // read `id` arg
-  // use `@node-cli-toolkit/oauth-cli` to log in
-  // save token using `@node-api-toolkit/save-token` to a file
-  // with the `id` arg as the name in `/tmp`
-});
